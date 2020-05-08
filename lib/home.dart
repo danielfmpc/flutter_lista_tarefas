cd0d6e90322656f4d1f49e58e933eef29474a43f
Daniel Fernando <daniel_fmpc@hotmail.com.br>
2020-05-08 18:20:37 -0300

    Criando Map e Listando ToDo

--- lib/home.dart
+++ lib/home.dart
@@ -10,7 +10,18 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  final _textEditingController = TextEditingController();
   List _toDoList = [];
+
+  void _addToDoList(){
+    setState(() {
+      Map<String, dynamic> newToDo = Map();
+      newToDo["title"] = _textEditingController.text;
+      _textEditingController.text = "";
+      newToDo["ok"] = false;
+      _toDoList.add(newToDo);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,36 +31,57 @@ class _HomeState extends State<Home> {
         backgroundColor: Colors.blueAccent,
         centerTitle: true,
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            Container(
-              padding: EdgeInsets.fromLTRB(17, 1, 7, 1),
-              child: Row(
-                children: <Widget>[
-                  Expanded(
-                    child: TextField(
-                      decoration: InputDecoration(
-                        labelText: "Nova Tarefa",
-                        labelStyle: TextStyle(
-                          color: Colors.blueAccent
-                        ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.fromLTRB(17, 1, 7, 1),
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: TextField(
+                    controller: _textEditingController,
+                    decoration: InputDecoration(
+                      labelText: "Nova Tarefa",
+                      labelStyle: TextStyle(
+                        color: Colors.blueAccent
                       ),
                     ),
                   ),
-                  RaisedButton(
-                    child: Text(
-                      "Crir"                      
-                    ),
-                    textColor: Colors.white,
-                    color: Colors.blueAccent,
-                    onPressed: (){}
+                ),
+                RaisedButton(
+                  child: Text(
+                    "Crir"                      
                   ),
-                ],
-              ),
+                  textColor: Colors.white,
+                  color: Colors.blueAccent,
+                  onPressed: _addToDoList,
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              padding: EdgeInsets.only(top: 10),
+              itemCount: _toDoList.length,
+              itemBuilder: (context, index){
+                return CheckboxListTile(
+                  title: Text(_toDoList[index]["title"]),
+                  value: _toDoList[index]["ok"],
+                  onChanged: (value){
+                    setState(() {
+                      _toDoList[index]["ok"] = value;
+                    });
+                  },
+                  secondary: CircleAvatar(
+                    child: Icon(
+                      _toDoList[index]["ok"] ? Icons.check : Icons.error
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
